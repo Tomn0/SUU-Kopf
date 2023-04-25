c504f76aac2a99b7957df1f7e8193137e912bc4a
Tomn0 <nal.tomek@gmail.com>
2023-04-25 14:41:06 +0200

    basic workers deployment

--- operator/rsacracker.py
+++ operator/rsacracker.py
@@ -8,3 +8,15 @@ import logging
 def create_fn(status, **kwargs):
     logging.info(f"A create handler is called")
 
+@kopf.on.create("pod", labels={ 'mylabel': 'temp-worker' })
+def create_temp_worker(status, **kwargs):
+    logging.info(f"TEMP worker created")
+
+'''
+TODO:
+[] napisać yaml z workera
+[] odpalić yamle z workerami jako 3 osobne deploymenty
+[] operator on_create wykrywa powstanie workerów
+[] dobić się do workerów
+[] 
+'''
--- worker/worker.py
+++ worker/worker.py
@@ -1,3 +1,5 @@
 # TODO
 # Code of the worker pod created by the operator
-print('Hello world from the worker')
+print('Hello world from the TEMP worker')
+while True:
+    pass
