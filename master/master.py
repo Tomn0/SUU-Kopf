e188458a0db2f36c7864522013535c4384d5f7ed
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-04-25 20:11:20 +0200

    Add basic flask API

--- master/master.py
+++ master/master.py
@@ -1,3 +1,44 @@
-# TODO
-# Code of the master pod created by the operator
-print('Hello world from the master')
+from flask import Flask, request, jsonify
+import time
+from enum import IntEnum
+
+app = Flask(__name__)
+
+
+
+class TaskStatus(IntEnum):
+    CREATED = 1
+    WORKING = 2
+    DONE = 3
+
+def deployTask(n):
+    taskID = str(time.time())
+    return taskID
+
+def getTask(taskID):
+    status = TaskStatus.CREATED
+    result = None
+
+    return {
+        'status': status,
+        'result': result,
+    }
+
+@app.route('/task', methods=['POST'])
+def create_task():
+    n = int(request.form.get('n'))
+    task_id = deployTask(n)
+    
+    return jsonify({'task_id': task_id}), 202
+
+@app.route('/task/<task_id>', methods=['GET'])
+def get_task(task_id):
+    task = getTask(task_id)
+
+    if task is None:
+        return jsonify({'error': 'Task not found'}), 404
+
+    return jsonify(task), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
