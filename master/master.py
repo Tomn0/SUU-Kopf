797dba36931aa3bfc2b0a89f0950a5c6bfa353a2
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-05-25 21:45:29 +0200

    Add test server to master

--- master/master.py
+++ master/master.py
@@ -1,3 +1,13 @@
-# TODO
-# Code of the master pod created by the operator
-print('Hello world from the master')
+from flask import Flask, request, jsonify
+import time
+import requests
+from enum import IntEnum
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'text': 'Hello world!'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
