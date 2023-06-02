b88a40e6bbe1c01c08c9fad1d0d9cec89471eecb
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-06-02 13:48:32 +0200

    Add communication master <-> operator

--- master/master.py
+++ master/master.py
@@ -2,44 +2,22 @@ from flask import Flask, request, jsonify
 import time
 import requests
 from enum import IntEnum
+import os
+
+operator_ip = os.environ.get('OPERATOR_IP', None)
+
+if operator_ip is None:
+    print('operator_ip env var not found in env!')
 
 app = Flask(__name__)
 
-start_task_endpoint = 'htttp://10.103.170.51:80/start-task'
-
-class TaskStatus(IntEnum):
-    CREATED = 1
-    WORKING = 2
-    DONE = 3
-
-def get_task(taskID):
-    status = TaskStatus.CREATED
-    result = None
-
-    return {
-        'status': status,
-        'result': result,
-    }
-
-@app.route('/task', methods=['POST'])
-def create_task():
-    n = int(request.form.get('n'))
-
+@app.route('/')
+@app.route('/progress')
+def progress():
     # send start-task request to operator
-    response = requests.post(start_task_endpoint, json={"n" : n})
+    response = requests.get('http://' + operator_ip + ':8080/progress')
 
-    # return created task's id
-    task_id = response.json()['task_id']
-    return jsonify({'task_id': task_id}), 202
-
-@app.route('/task/<task_id>', methods=['GET'])
-def get_task(task_id):
-    task = get_task(task_id)
-
-    if task is None:
-        return jsonify({'error': 'Task not found'}), 404
-
-    return jsonify(task), 200
+    return response.json(), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
