cd2f77169313340f1c92e3e90818fe621a58f64b
Tomn0 <nal.tomek@gmail.com>
2023-06-02 12:04:16 +0200

    fixed worker not detecting the solution

--- worker/worker.py
+++ worker/worker.py
@@ -1,10 +1,12 @@
 import time
+import sys
 from flask import Flask
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
 import random
 from state import State, create_initial_state
 import jsonpickle
+import logging
 
 # env var init and global vars
 id = os.environ.get('ID', None)
@@ -22,23 +24,9 @@ salt = random.randint(1, 9999999) # random salt to generare a unique backup file
 # flask init
 app = Flask(__name__)
 
-# background task init
-def bg_task():
-    global state
-    
-    while state.current_number <= state.last_number:
-        if state.current_number % state.N == 0:
-            state.solution = state.current_number
-            save()
-            return
-        
-        state.current_number += 1
-    
-    save()
-
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(bg_task)
-scheduler.start()
+# set logs to sys.stdout
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.INFO)
 
 # flask endpoints
 @app.route("/increment")
@@ -52,7 +40,7 @@ def debug():
     return str(state)
 
 @app.route("/save")
-def save():
+def save(solution=False):
     global id, state
 
     # create directory for this worker
@@ -60,9 +48,38 @@ def save():
     if not os.path.exists(path):
         os.makedirs(path)
 
+    if solution:
+        with open(f'{path}/solution.dat', 'w') as file:
+            state_string = jsonpickle.encode(state)
+            file.write(f'{state_string}')
+
     # save state to file
     with open(f'{path}/{salt}.dat', 'w') as file:
         state_string = jsonpickle.encode(state)
         file.write(f'{state_string}')
 
     return 'State saved correctly'
+
+# background task init
+def bg_task():
+    global state
+    
+    while state.current_number <= state.last_number:
+        # print(f"print: Trying number: {state.current_number}")
+        # logging.info(f"Trying number: {state.current_number}")
+        if state.N % state.current_number == 0:
+            state.solution = state.current_number
+            # print(f"print: Found solution {state.solution}")
+            # logging.info(f"Found solution {state.solution}")
+            save(solution=True)
+            return
+        # time.sleep(10)
+        state.current_number += 1
+
+    
+    save()
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(bg_task)
+scheduler.start()
+
